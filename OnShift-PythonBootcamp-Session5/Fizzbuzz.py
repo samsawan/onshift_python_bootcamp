0973a537fb0e3c085a5fa1232751dcb8f8e9e123
Sam Sawan <ssawan@onshift.com>
2018-03-06 09:22:51 -0500

    remove venvs

--- OnShift-PythonBootcamp-Session5/Fizzbuzz.py
+++ OnShift-PythonBootcamp-Session5/Fizzbuzz.py
@@ -1,6 +1,6 @@
 class Fizzbuzz:
 
-    def get_fizzbuzz_string(self,input_num):
+    def get_fizzbuzz_string(self, input_num):
         if input_num % 3 == 0 and input_num % 5 == 0:
             return "Fizzbuzz"
         elif input_num % 3 == 0:
