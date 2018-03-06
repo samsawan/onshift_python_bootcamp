ef2cd1c1daf226f38d465d3182f117cf8c4775fb
Sam Sawan <ssawan@onshift.com>
2018-03-06 09:57:27 -0500

    cleanup the api consumption module

--- OnShift-PythonBootcamp-Session5/APIConsumption.py
+++ OnShift-PythonBootcamp-Session5/APIConsumption.py
@@ -1,7 +1,6 @@
 # Using the requests module
 import requests
-# Bring in JSON to do some converting
-import json
+
 # Star Wars API - https://swapi.co/
 api_url = 'https://swapi.co/api/'
 
@@ -13,12 +12,12 @@ response = requests.get(api_url + 'starships/9')
 starship9_data = response.json()
 
 # Print the status code of the response.
-print(response.status_code)
+print("response code from endpoint that exists: {}\n".format(response.status_code))
 
 # File Not Found
 # Note that the / before starships is intentional
-response = requests.get(api_url + '/starships/9')
-print(response.status_code)
+response = requests.get(api_url + 'dne')
+print("response code from endpoint that dne: {}\n".format(response.status_code))
 
 
 # Set up the parameters we want to pass to the API.
@@ -29,16 +28,16 @@ parameters = {'format': 'wookiee'}
 response = requests.get(api_url + 'starships/9/', params=parameters)
 
 # Print the content of the response (the data the server returned)
-print(response.content)
+print("wookie format response: {}\n".format(response.content))
 
 # This gets the same data as the command above
 response = requests.get(api_url + 'starships/9/?format=wookiee')
-print(response.content)
+print("another way to get wookie data: {}\n".format(response.content))
 
 # What is starship 9?
-print(starship9_data['name'])
+print("startship9 name: {}\n".format(starship9_data['name']))
 # Who is the manufacturer?
-print(starship9_data['manufacturer'])
+print("starship9 manufacturer: {}\n".format(starship9_data['manufacturer']))
 
 # JSON representation of starship 9
-print(starship9_data)
+print("all of the data associated with starship9: {}\n".format(starship9_data))
